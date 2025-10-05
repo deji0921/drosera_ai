5f731bcdab1d8152d433d158b0b27d6622ce2429
deji0921 <140453885+deji0921@users.noreply.github.com>
2025-10-05 14:26:27 +0100

    Delete data directory

--- data/questions.ts
+++
@@ -1,9 +0,0 @@
-const quickQuestions: string[] = [
-  "What is Drosera Network?",
-  "How do Traps work?",
-  "How to integrate Drosera?",
-  "What attacks can it prevent?",
-  "What makes it trustless?",
-];
-
-export { quickQuestions };
